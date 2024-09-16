88fcd4e152812bbcd7742f6ad2d2c48c2297733e
Muhammad Shoaib <muhammad.shoaib@focusitservices.co.uk>
2024-09-16 21:39:18 +0500

    User Authentication implemented login, logout, register, password reset with routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// throttling middleware to rate limit on requests
+Route::middleware('throttle:60,1')->group(function () {
+    Route::post('register', [UserController::class, 'register']); 
+    Route::post('login', [UserController::class, 'login']);
+    Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum'); //use auth sanctum middleware for authorized users
+    Route::post('forgot-password', [UserController::class, 'sendResetLink']);
+    Route::post('reset-password', [UserController::class, 'resetPassword']);
+});
